b974fefabc01ef012f9576a027f88d4867b568f1
MRavindu <ravindumedagama@gmail.com>
2025-05-10 13:58:55 +0530

    Started Creating the Game Site

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Home from './Pages/Home'
 import Header from './Components/Header'
 
 function App() {
-
+  const [theme, setTheme] = useState('dark')
 
   return (
-    <>
+    <div className={`${theme} ${theme === 'dark' ? 'bg-[#121212]' : ''}`}>
       <Header/>
       <Home/>
-    </>
+    </div>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
-import Logo from '../assets/logo.svg'
-import { HiMiniMagnifyingGlassCircle, HiMoon } from "react-icons/hi2";
+import React from "react";
+import Logo from "../assets/logo.svg";
+import { HiMiniMagnifyingGlassCircle, HiMoon, HiSun } from "react-icons/hi2";
+import { useState } from "react";
 
 function Header() {
+  //
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <div className='flex items-center'>
-        <img src={Logo} alt="Logo" width={60} />
-        <div className='flex bg-slate-200 p-2 w-full
-        mx-5 rounded-full items-center'>
-            <HiMiniMagnifyingGlassCircle />
-            <input type="text" placeholder="Search games..." className='px-5 bg-transparent outline-none'/>
-        </div>
+    <div className="flex items-center p-5">
+      <img src={Logo} alt="Logo" width={50} />
+      <div className="flex bg-slate-200 p-2 w-full mx-5 rounded-full items-center">
+        <HiMiniMagnifyingGlassCircle />
+        <input
+          type="text"
+          placeholder="Search Games"
+          className="px-5 bg-transparent dark:bg-cyan-500 outline-none"
+        />
+      </div>
 
-        <div>
-            <HiMoon/>
-        </div>
-
+      <div>
+        {darkMode ? (
+          <HiMoon
+            className="text-[35px] bd-slate-200 text-black p-1 rounded-full cursor-pointer"
+            onClick={() => setDarkMode(!darkMode)}
+          />
+        ) : (
+          <HiSun
+            className="text-[35px] bd-slate-200 text-black p-1 rounded-full cursor-pointer"
+            onClick={() => setDarkMode(!darkMode)}
+          />
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
